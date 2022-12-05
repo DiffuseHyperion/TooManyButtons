6d0428efcbf650336944fa207df42fed7532e449
DiffuseHyperion <xieyingjie08@gmail.com>
2022-12-05 19:03:11 +0800

    remove unneeded accesswidener
    preliminary icon style mod menu

--- src/main/java/tk/diffusehyperion/toomanybuttons/gui/EditGameScreen.java
+++ src/main/java/tk/diffusehyperion/toomanybuttons/gui/EditGameScreen.java
@@ -1,7 +1,9 @@
 package tk.diffusehyperion.toomanybuttons.gui;
 
+import com.terraformersmc.modmenu.config.ModMenuConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import org.javatuples.Pair;
 import org.javatuples.Tuple;
 import org.javatuples.Unit;
@@ -9,6 +11,7 @@ import org.javatuples.Unit;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.terraformersmc.modmenu.config.ModMenuConfig.MODS_BUTTON_STYLE;
 import static tk.diffusehyperion.toomanybuttons.config.ClothConfigHandler.*;
 import static tk.diffusehyperion.toomanybuttons.config.ClothConfigHandler.HIDE_MODMENU_GAMEMENU;
 
@@ -21,14 +24,16 @@ public class EditGameScreen extends ScreenWidgetEditor{
     }
 
     public void main() {
-        moveDownKeyedWidget("modmenu.title", 48);
-        moveUpKeyedWidget("menu.options", 24);
-        if (client.isInSingleplayer()) {
-            moveUpKeyedWidget("menu.shareToLan", 24);
-            moveUpKeyedWidget("menu.returnToMenu", 24);
-        } else {
-            moveUpKeyedWidget("menu.playerReporting", 24);
-            moveUpKeyedWidget("menu.disconnect", 24);
+        if (MODS_BUTTON_STYLE.getValue().equals(ModMenuConfig.ModsButtonStyle.CLASSIC)) {
+            moveDownKeyedWidget("modmenu.title", 48);
+            moveUpKeyedWidget("menu.options", 24);
+            if (client.isInSingleplayer()) {
+                moveUpKeyedWidget("menu.shareToLan", 24);
+                moveUpKeyedWidget("menu.returnToMenu", 24);
+            } else {
+                moveUpKeyedWidget("menu.playerReporting", 24);
+                moveUpKeyedWidget("menu.disconnect", 24);
+            }
         }
         // for some reason, mod menu button is smack in the middle of the menu between feedback and options,
         // so the mod moves it to below quit to title and move everything below original mod menu upwards
@@ -44,7 +49,14 @@ public class EditGameScreen extends ScreenWidgetEditor{
             widgetList.add(new Pair<>(new Pair<>(HIDE_OPTIONS_GAMEMENU, getWidget("menu.options")), new Pair<>(HIDE_REPORTING, getWidget("menu.playerReporting"))));
             widgetList.add(new Unit<>(new Pair<>(HIDE_DISCONNECT, getWidget("menu.disconnect"))));
         }
-        widgetList.add(new Unit<>(new Pair<>(HIDE_MODMENU_GAMEMENU, getWidget("modmenu.title"))));
-        editWidgetScreen(widgetList, 205, 106);  //250 120
+        if (MODS_BUTTON_STYLE.getValue().equals(ModMenuConfig.ModsButtonStyle.CLASSIC)) {
+            widgetList.add(new Unit<>(new Pair<>(HIDE_MODMENU_GAMEMENU, getWidget("modmenu.title"))));
+        }
+        int yOffset = editWidgetScreen(widgetList, 205, 106);
+        if (HIDE_MODMENU_GAMEMENU) {
+            ClickableWidget widget = getWidget("modmenu.title");
+            widget.visible = false;
+            widget.y -= yOffset;
+        }
     }
 }
